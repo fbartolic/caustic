d8591c08febe04d0d3a9da7ca5e03a538fd4827e
Fran Bartolic <fb90@st-andrews.ac.uk>
2019-09-16 15:55:15 +0100

    added configuration file for notebooks

---
+++ docs/examples/notebook_config.py
@@ -0,0 +1,31 @@
+get_ipython().magic("matplotlib inline")
+get_ipython().magic('config InlineBackend.figure_format = "retina"')
+
+# Hide deprecation warnings from Theano
+import warnings
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+warnings.filterwarnings("ignore", category=FutureWarning)
+
+# Hide Theano compilelock warnings
+import logging
+
+logger = logging.getLogger("theano.gof.compilelock")
+logger.setLevel(logging.ERROR)
+
+import theano
+
+print("theano version: {0}".format(theano.__version__))
+
+import pymc3
+
+print("pymc3 version: {0}".format(pymc3.__version__))
+
+import matplotlib.pyplot as plt
+
+plt.style.use("default")
+plt.rcParams["savefig.dpi"] = 100
+plt.rcParams["figure.dpi"] = 100
+plt.rcParams["font.size"] = 16
+plt.rcParams["font.sans-serif"] = ["Liberation Sans"]
+plt.rcParams["mathtext.fontset"] = "custom"
