f9e45b87ef211302b2b6b33b137512def81bf743
Fran Bartolic <fb90@st-andrews.ac.uk>
2019-09-16 15:39:30 +0100

    removing unnecessary files

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ nbsphinx_prolog = r"""
 
         This page was generated from `{{ docname }}`__.
         Interactive online version:
-        :raw-html:`<a href="https://mybinder.org/v2/gh/fbartolic/caustic/{{ env.config.release }}?filepath={{ docname }}"><img alt="Binder badge" src="https://mybinder.org/badge_logo.svg" style="vertical-align:text-bottom"></a>`
+        :raw-html:`<a href="https://mybinder.org/v2/gh/fbartolic/caustic/blob/master/{{ env.config.release }}?filepath={{ docname }}"><img alt="Binder badge" src="https://mybinder.org/badge_logo.svg" style="vertical-align:text-bottom"></a>`
 
     __ https://github.com/fbartolic/caustic
         {{ env.config.release }}/{{ docname }}
