cae4ab39c2f098e0c8c495b53c01a9823f2b6ad5
Fran Bartolic <fb90@st-andrews.ac.uk>
2019-11-29 21:08:37 +0000

    test no longer needed

--- caustic/tests/test_utils.py
+++ caustic/tests/test_utils.py
@@ -11,26 +11,6 @@ from caustic import (
 
 np.random.seed(42)
 
-
-def test_construct_masked_tensor():
-    # Create list of numpy arrays of varying lengths
-    a = np.random.rand(1000)
-    b = np.random.rand(600)
-    c = np.random.rand(800)
-
-    list_np = [a, b, c]
-
-    tensor, mask = construct_masked_tensor(list_np)
-
-    # Check if shapes are correct
-    assert T.shape(tensor).eval()[0] == 3
-    assert T.shape(tensor).eval()[1] == 1000
-
-    # Convert back to numpy
-    for i in range(3):
-        assert np.allclose(list_np[i], tensor[i][mask[i].nonzero()].eval())
-
-
 def test_get_log_likelihood_function():
     # Create trivial model
     with pm.Model() as model:
